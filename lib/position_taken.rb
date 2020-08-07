c29862dc455362bc477ae3bdf539ae0fe76825fd
Rosario Holsen <rholsen@gmail.com>
2020-08-07 16:08:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true 
-  else index == " " || index == ""
+  else board[index] == " " || board[index] == ""
     
   end
 end
