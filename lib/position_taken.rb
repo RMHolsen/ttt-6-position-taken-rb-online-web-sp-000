6b1bd5d72ee29b0e29a97b11c0d211f52ee21879
Rosario Holsen <rholsen@gmail.com>
2020-08-07 16:00:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if index == "X" || index == "O"
-    true
+  
   elsif index == " " || index == ""
-    false
+    
   end
 end
